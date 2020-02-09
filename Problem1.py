6ccaa7baba8c08ca983fba1ca9d72a9ecbe45220
Fathu1998 <60419774+Fathu1998@users.noreply.github.com>
2020-02-09 15:03:14 +0530

    Add files via upload

---
+++ Problem1.py
@@ -0,0 +1,5 @@
+a=3**2 #This is the square of one side
+b=5**2 #This is the square of other side
+c=a+b #This is their sum
+print(c)
+
---
+++ Problem2.py
@@ -0,0 +1,7 @@
+s='TwinkletwinklelittlestarHowiwonderwhatyouare'#This is a string
+a= 0 #a,b,c,d are integers corresponding to letters in the string
+b= 7
+c= 8
+d= 14
+print(s[a:b]+" "+s[c-1:d])#This will give the required output
+
